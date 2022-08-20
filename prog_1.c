23afd258a998b8f2977ef41c3fac1ba59a734a36
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-08-20 14:33:37 +0530

    length of string

--- prog_1.c
+++ prog_1.c
@@ -1,1 +1,17 @@
+/*Write a function to calculate length of the string*/
+#include <stdio.h>
 
+int main() {
+    char str[50];
+    int i;
+    printf("Enter the input string: ");
+    fgets(str, 50, stdin);
+    
+    printf("Output string is: %s", str);
+    
+    for(i=0; str[i]; i++);
+    printf("\nLength of string is : %d", i);
+    
+
+    return 0;
+}
